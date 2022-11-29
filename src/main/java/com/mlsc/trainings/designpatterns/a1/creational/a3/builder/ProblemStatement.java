f920bffc6f9736e2d2a814e49df175fe43d24e37
Muhammed Shakir <shakir@infomover.io>
2022-11-29 14:36:35 +0530

    Builder in progress !

---
+++ src/main/java/com/mlsc/trainings/designpatterns/a1/creational/a3/builder/ProblemStatement.java
@@ -0,0 +1,18 @@
+package com.mlsc.trainings.designpatterns.a1.creational.a3.builder;
+
+/**
+ * <p>Imagine a complex object that requires laborious, step-by-step initialization of
+ * many fields and nested objects. Such initialization code is usually buried inside a
+ * monstrous constructor with lots of parameters.
+ * Or even worse: scattered all over the client code.
+ * </p>
+ * <p>For example, let’s think about how to create a House object.
+ * To build a simple house, you need to construct four walls and a floor,
+ * install a door, fit a pair of windows, and build a roof. But what if you want a bigger,
+ * brighter house, with a backyard and other goodies
+ * (like a heating system, plumbing, and electrical wiring)?
+ * </p>
+ */
+public class ProblemStatement {
+
+}
