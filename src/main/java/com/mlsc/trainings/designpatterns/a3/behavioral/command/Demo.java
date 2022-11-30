7bdad70071f7680a1b825bcf838d297fd3f29e72
Muhammed Shakir <shakir@infomover.io>
2022-11-30 16:53:51 +0530

    Command explained !

---
+++ src/main/java/com/mlsc/trainings/designpatterns/a3/behavioral/command/Demo.java
@@ -0,0 +1,8 @@
+package com.mlsc.trainings.designpatterns.a3.behavioral.command;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
